4205a44c78f565c068ce2b4c925ca50ab1919a4a
Abhinav3021 <124438690+Abhinav3021@users.noreply.github.com>
2025-04-13 19:39:30 +0530

    second Commit

--- expense/tracker/urls.py
+++ expense/tracker/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from tracker.views import index, deleteTransaction
+from tracker.views import index, deleteTransaction, registration,login_page,logout_page
 
 urlpatterns=[
     path('',index,name="index"),
-    path('delete-tranaction/<uuid>/',deleteTransaction,name='deleteTransaction')
+    path('delete-tranaction/<uuid>/',deleteTransaction,name='deleteTransaction'),
+    path('registration/',registration, name='registation'),
+    path('login/',login_page, name="login_page"),
+    path('logout/',logout_page,name='logout_page')
 ]
--- expense/tracker/views.py
+++ expense/tracker/views.py
@@ -2,9 +2,76 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from tracker.models import Transaction
 from django.db.models import Sum
-
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+def registration(request):
+    if(request.method=='POST'):
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        user_obj= User.objects.filter(
+            Q(email=email) | Q(username=username)
+        )
+        
+        if user_obj.exists():
+            messages.error(request,'Error: Username or Email already exists')
+            return redirect('/registration/')
+        
+        user_obj=User.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=email
+        )
+        user_obj.set_password(password)
+        user_obj.save()
+        messages.error(request,'Success: Account Created')
+        return redirect('/registration/')
+    
+    
+    return render(request,'register.html')
+
+
+def login_page(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user_obj = User.objects.filter(
+          username = username
+            )
+        if not user_obj.exists():
+            messages.error(request, 'Error: Username does not exist')
+            return redirect('/login/')
+
+       # as the password is encrypted , we can't check by (=)....so we need to use authenticate function
+        user_obj = authenticate(username = username , password = password) # authenticate will return the object if true
+
+        if not user_obj:
+            messages.error(request, 'Error: Invalid credentials')
+            return redirect('/login/')
+        
+        login(request, user_obj) # Genrate the cookie used by django session
+        return redirect('/') # redirect to the main page
+        
+
+    return render(request , 'login.html')
+    
+
+def logout_page(request):
+    logout(request)
+    messages.error(request,"Success: LogOut Successfull")
+    return redirect('/login/')
+
+@login_required(login_url='/login/')  # decorater for protecting the routes
 def index(request):
+    #print(request.user)
     if(request.method=="POST"):
         description=request.POST.get('description')
         amount=request.POST.get('amount')
@@ -25,18 +92,20 @@ def index(request):
         Transaction.objects.create(
             description=description,
             amount=amount,
+            created_by=request.user
         )
     
         return redirect('/')
     
-    context={'transcations':Transaction.objects.all(),
-             'balance':Transaction.objects.all().aggregate(total_balance=Sum('amount'))['total_balance'] or 0,
-             'income':Transaction.objects.filter(amount__gte=0).aggregate(income=Sum('amount'))['income'] or 0,
-             'expanse':Transaction.objects.filter(amount__lte=0).aggregate(expanse=Sum('amount'))['expanse'] or 0,
+    context={'transcations':Transaction.objects.filter(created_by=request.user),
+             'balance':Transaction.objects.filter(created_by=request.user).aggregate(total_balance=Sum('amount'))['total_balance'] or 0,
+             'income':Transaction.objects.filter(created_by=request.user,amount__gte=0).aggregate(income=Sum('amount'))['income'] or 0,
+             'expanse':Transaction.objects.filter(created_by=request.user,amount__lte=0).aggregate(expanse=Sum('amount'))['expanse'] or 0,
              }
     #print(context)
     return render(request,'index.html',context) #In Django, context is a dictionary that contains data you want to pass from your view to your template. This data can then be accessed and displayed in the template.
 
+@login_required(login_url='/login/') 
 def deleteTransaction(request,uuid):
     Transaction.objects.get(uuid=uuid).delete()
     return redirect('/')
